d7c7750cb5ae9999d16a8abc61411ca0056c71b9
Jase Dee <jasecdee@icloud.com>
2025-06-04 16:13:37 -0600

    change cli and add guide + ideas for scanning/saving wifi info

--- WifiProbe.py
+++ WifiProbe.py
@@ -1,13 +1,19 @@
-#import subprocess
+import subprocess
 import sys
 import os
 #import argparse -alternative option for getting cli args might need
 
+def user_guide(): #guide with usage instructions
+	guide = """
+Usage Guide: python3 WifiProbe.py --capture --intf <interface> --output <snapshot.py>
 
-	print(" Example input: python3 WifiProbe.py --capture --intf <interface> --output <snapshot.py> ") #some cli args to make mock snapshot of lab
-	print(" --capture (Required) --intf (Required) --output (not necessary) ")
+Arguments:
 
-def user_guide(): #make reusable guide with instructions for use and to follow all error messages
+--capture		Argument to capture a snapshot (required)
+--intf <interface>      Argument for interface (ex. wlan0) (required)
+--output <file>		Argument to name the output file (default: snapshot.py)
+"""
+	print(guide)
 
 def parse_cli():
 
@@ -21,30 +27,42 @@ def parse_cli():
 		arg = args[i]
 		if arg == '--capture': #check for --capture
 			capture = True
-			i += 1;
+			i += 1
 		else if arg == '--intf': #check for -intf and a value
 			if i + 1 < len(args):
 				intf = args[i+1]
 				i += 2
 			else
 				print("Error: --intf requires a value. Ex. --intf <wlan0>")
+				user_guide()
 				sys.exit(1)
-		else if arg == '--output':
+		else if arg == '--output': #check for --output and a value
 			if i + 1 < len(args):
 				output = args[i+1]
 				i += 2
 			else
-				printf("Error: --output requires a value. Ex. --output <airgeddon_snapshot1.py>")
+				print("Error: --output requires a value. Ex. --output <airgeddon_snapshot1.py>")
+				user_guide()
+				sys.exit(1)
 		else
 			print("Error: Unknown cli argument")
+			user_guide()
 			sys.exit(1)
 
 	return capture, intf, output
 
-#def extract_lab(intf): pull data from lab
+#def extract_intf_data(intf): pull network data
+	# option iw intf scan - output = subprocess.check_output(['sudo', 'iw', intf, 'scan'], text=True)
+	# for essid, channel, signal, encyption
+	#library backend libnl for parsing insteaad of iw or libiw
+	#json
+
+#def pars_airmon-ng_output():
+	#parse airmon-ng? - phys, intf, driver, chipset
 
 #def parse_output(     ): read and store data
 
-#def gen_snapshot(     ): generate snapshot
+
+#def gen_snapshot(     ): generate snapshot to labs folder
 
 #def main():
