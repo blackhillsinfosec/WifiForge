07b6740f1d83378ecf7b5960d9fcc715ee25f214
hun <96307763+hundotio@users.noreply.github.com>
2025-06-16 14:38:14 -0500

    updated drone lab for nc listeners (add path handling later)
    - figured out cmd injection for nodes
    - added NAT for dr1 and dr2
    - cmd injection for custom userland shell program
    - updated info file, combined positions and passwords to drone-info.json

--- framework/labs/drone_hacking.py
+++ framework/labs/drone_hacking.py
@@ -19,8 +19,7 @@ def drones():
     # configure drone names and passwords here
     dronepass = {
         "dr1": "password!",
-        "dr2": "arcangel",
-        "dr3": "letmein",
+        "dr2": "arcangel"
     }
     
     print("Creating Attacker Station...")
@@ -29,60 +28,77 @@ def drones():
     print("Creating Drones and Controllers...")
 
     # drone 1 network
-    cr1 = net.addStation('cr1', passwd=dronepass["dr1"], encrypt='wpa2', mac='00:00:00:00:00:01')
-    dr1 = net.addAccessPoint('dr1', ssid='DRONE1', passwd=dronepass["dr1"], encrypt='wpa2', mode='g', channel='3')
-
+    st1 = net.addStation('st1', passwd=dronepass["dr1"], encrypt='wpa2', mac='00:00:00:00:00:01', ip='10.1.0.10/24')
+    dr1 = net.addAccessPoint('dr1', ssid='DRONE1', passwd=dronepass["dr1"], encrypt='wpa2', mode='g', channel='3', failMode="standalone")
+    
     # drone 2 network
-    cr2 = net.addStation('cr2', passwd=dronepass["dr2"], encrypt='wpa2', mac='00:00:00:00:00:02')
-    dr2 = net.addAccessPoint('dr2', ssid='DRONE2', passwd=dronepass["dr2"], encrypt='wpa2', mode='g', channel='6')
+    st2 = net.addStation('st2', passwd=dronepass["dr2"], encrypt='wpa2', mac='00:00:00:00:00:02', ip='10.2.0.10/24')
+    dr2 = net.addAccessPoint('dr2', ssid='DRONE2', passwd=dronepass["dr2"], encrypt='wpa2', mode='g', channel='6', failMode="standalone")
 
-    # drone 3 network
-    cr3 = net.addStation('cr3', passwd=dronepass["dr3"], encrypt='wpa2', mac='00:00:00:00:00:03')
-    dr3 = net.addAccessPoint('dr3', ssid='DRONE3', passwd=dronepass["dr3"], encrypt='wpa2', mode='g', channel='11')
+    # net.configureWifiNodes()
+    net.configureNodes()
 
-    net.configureWifiNodes()
-
-    # build out the network 
+    # build out the network
     print('Configuring Network...')
-    net.addLink(cr1, dr1)
-    net.addLink(cr2, dr2)
-    net.addLink(cr3, dr3)
+    net.addLink(dr1, st1)
+    net.addLink(dr2, st2)
+    
+    # configure NAT to dr1 and dr2
+    nat1 = net.addNAT(name='nat1')
+    nat2 = net.addNAT(name='nat2')
+
+    net.addLink(nat1, dr1)
+    net.addLink(nat2, dr2)
+    
+    # LAN address, WAN is applied automatically
+    nat1.setIP('10.1.0.254/24', intf='nat1-eth1')
+    nat2.setIP('10.2.0.254/24', intf='nat2-eth1')
+    
+    # configure routing for the stations
+    st1.cmd('ip route add default via 10.1.0.254')
+    st2.cmd('ip route add default via 10.2.0.254')
+
+    for n in [nat1, nat2]:
+        n.cmd('sysctl -w net.ipv4.ip_forward=1')
+        n.cmd('iptables -t nat -A POSTROUTING -o nat1-eth0 -j MASQUERADE') 
 
     net.build()
     dr1.start([])
     dr2.start([])
-    dr3.start([])
 
     # set the drone positions (x, y, z)
     dr1.setPosition('20,20,0')
     dr2.setPosition('10,28,0')
-    dr3.setPosition('4,15,0')
-    
-    # write the drone names and positions to a JSON dictionary (/tmp/drone_positions.json)
-    positions = {}
-    for n in net.aps:
-        positions[n.name] = [float(x) for x in n.position]
-    with open('/tmp/drone_positions.json', 'w') as f:
-        json.dump(positions, f)
 
-    # write the drone passwords to a JSON dictionary (/tmp/drone_passwords.json)
-    passwords = {}
+    # userland listener service on port 4444, write the password for the drone
+    dr1.cmd('python3 shell.py', dronepass["dr1"], '&')
+    dr2.cmd('python3 shell.py', dronepass["dr2"], '&')
+
+    # write drone information for controllers and graph (/tmp/drone-info.json)
+    info = {}
+
+    # write ap/drone positions 
     for n in net.aps:
-        if n.name in dronepass:
-            passwords[n.name] = [dronepass[n.name]]
-    with open('/tmp/drone_passwords.json', 'w') as f:
-        json.dump(passwords, f)
-    
+        info[n.name] = {
+            "position": [float(x) for x in n.position],
+            "password": [dronepass[n.name]]
+        }
+    print(info)
+    with open('/tmp/drone-info.json', 'w') as f:
+        json.dump(info, f)
+
     spin.stop()
+    # CLI(net)
     CONFIG_TMUX(["Attacker", "Attacker", "Attacker"], "DRONES")
 
+    print("Stopping Network...")
     net.stop()
     os.system("clear")
 
     # remove the tmp files (cleanup)
-    for path in ['/tmp/drone_positions.json', '/tmp/drone_passwords.json']:
-        if os.path.exists(path):
-            os.remove(path)
+    path = '/tmp/drone-info.json'
+    if os.path.exists(path):
+        os.remove(path)
 
 if __name__ == '__main__':
     drones()
